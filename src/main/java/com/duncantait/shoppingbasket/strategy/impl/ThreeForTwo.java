173f4bee1223bde5894faee3dc7db13fad4f032e
Duncan Tait <duncan.tait@hub.com>
2024-04-13 00:51:25 +0100

    Add docstring to 3 for 2

--- src/main/java/com/duncantait/shoppingbasket/strategy/impl/ThreeForTwo.java
+++ src/main/java/com/duncantait/shoppingbasket/strategy/impl/ThreeForTwo.java
@@ -9,6 +9,15 @@ public class ThreeForTwo implements PricingStrategy {
         this.pricePerUnit = pricePerUnit;
     }
 
+    /**
+     * Calculate the cost of the items based on the 3 for 2 pricing strategy
+     * 1. `quantity / 3`: Calculate the number of sets of 3 items
+     * 2. `* 2` : Multiply by 2 to get the cost of 2 items (remember it's 3 for 2, and this is the cost of 2)
+     * 3. `+ quantity % 3`: Add the cost of the remaining items (e.g. if there are 2 items left, add the cost of 2 items)
+     * 4. `* pricePerUnit`: Multiply by the price per unit to get the total cost
+     * @param quantity The number of items
+     * @return The total cost of the items
+     */
     @Override
     public int calculateCost(int quantity) {
         return (quantity / 3 * 2 + quantity % 3) * pricePerUnit;
