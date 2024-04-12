7350ada90d88833931877a71928f027e1e0fade4
Duncan Tait <duncan.tait@hub.com>
2024-04-13 00:53:22 +0100

    Add docstring to bogof

--- src/main/java/com/duncantait/shoppingbasket/strategy/impl/BuyOneGetOneFree.java
+++ src/main/java/com/duncantait/shoppingbasket/strategy/impl/BuyOneGetOneFree.java
@@ -9,6 +9,14 @@ public class BuyOneGetOneFree implements PricingStrategy {
         this.pricePerUnit = pricePerUnit;
     }
 
+    /**
+     * Calculate the cost of the items based on the buy-one-get-one-free pricing strategy
+     * 1. `quantity / 2`: Calculate the number of sets of 2 items
+     * 3. `+ quantity % 2`: Add the cost of the remaining items (i.e. if there is 1 item left after all the sets of 2 have been accounted for, add the cost of 1 item)
+     * 4. `* pricePerUnit`: Multiply by the price per unit to get the total cost
+     * @param quantity The number of items
+     * @return The total cost of the items
+     */
     @Override
     public int calculateCost(int quantity) {
         return (quantity / 2 + quantity % 2) * pricePerUnit;
